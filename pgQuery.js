39bed45d73cf0dc5c1414343d6b426061bb75962
x1 <ankaabdullah.id@gmail.com>
2023-01-24 10:18:47 +0700

    sel jan 24 - 2023

--- pgQuery.js
+++ pgQuery.js
@@ -19,6 +19,27 @@ const search = {
   // date: "createdAt between '2020-01-01 12:00:00' and '2020-01-01 23:30:00'",
 };
 
+const objLoop = (obj) => {
+  let str = "";
+  const operator = ["AND", "OR"];
+  Object.keys(obj).map((key) => {
+    const x = obj[key],
+      strQ = ` ${x[0]} ${table}.${key} = ${x[1]}`,
+      strIlike = `ILIKE '%${x[1]}%'`;
+
+    if (key === "default") str += "AND " + x;
+
+    if (operator.includes(x[0])) {
+      typeof x[1] === "number"
+        ? (str += strQ)
+        : (str += strQ.split("=")[0] + strIlike);
+    } else {
+      str += ` ${table}.${key} ` + strIlike;
+    }
+  });
+  return str;
+};
+
 const sqlInsertHelper = (table, d) => {
   let column = "",
     value = [];
@@ -26,67 +47,22 @@ const sqlInsertHelper = (table, d) => {
     column += `${key}, `;
     value.push(d[key]);
   });
-  let sql = `INSERT INTO ${table}(${column}) VALUES(${value
+  return `INSERT INTO ${table}(${column}) VALUES(${value
     .map((v) => `'${v}'`)
     .join(", ")})`;
-  return sql;
 };
 
 const sqlGetDataHelper = (table, search, limit, offset, orderBy) => {
-  let searchBy = "",
-    order = orderBy ? ` ORDER BY ${orderBy}` : "";
-  page = limit ? ` LIMIT ${limit} OFFSET ${offset || 0}` : "";
-  Object.keys(search).map((key) => {
-    const x = search[key];
-    switch (true) {
-      case key === "date":
-        searchBy += "AND " + x;
-        break;
-      case x[0] === "AND":
-        searchBy += `AND ${table}.${key} ILIKE '%${x[1]}%' `;
-        break;
-      case x[0] === "OR":
-        searchBy += `OR ${table}.${key} ILIKE '%${x[1]}%' `;
-        break;
-      default:
-        searchBy += `${table}.${key} ILIKE '%${x}%' `;
-    }
-  });
-  return `SELECT * FROM ${table} WHERE ${searchBy.slice(0, -4) + order + page}`;
-};
+  const order = orderBy ? ` ORDER BY ${orderBy}` : "",
+    page = limit ? ` LIMIT ${limit} OFFSET ${offset || 0}` : "",
+    searchBy = objLoop(search);
 
-const sqlCountHelper = (table, search) => {
-  let searchBy = "";
-  Object.keys(search).map((key) => {
-    key !== "date"
-      ? (searchBy += ` ${table}.${key} ILIKE '%${search[key]}%' AND`)
-      : (searchBy += search[key]);
-  });
-  return `SELECT count(*) FROM ${table} WHERE${searchBy.slice(0, -3)}`;
+  return `SELECT * FROM ${table} WHERE ${searchBy + order + page}`;
 };
 
 const update = (table, where, data) => {
-  let searchBy = "",
+  let searchBy = objLoop(where),
     arrUpdate = [];
-  // create 'where' sql query
-  Object.keys(where).map((key) => {
-    const x = where[key];
-
-    if (key === "date") {
-      searchBy += "AND " + x;
-    } else if (x[0] === "AND" || x[0] === "OR") {
-    /** create (AND, OR) operator query */
-      typeof x[1] === "number"
-        ? (searchBy += ` ${x[0]} ${table}.${key} = ${x[1]}`)
-        : (searchBy += ` ${x[0]} ${table}.${key} ILIKE '%${x[1]}%'`);
-    } else if (x[0] === "NOT") {
-        /** create (NOT) operator query */
-      searchBy += ` ${x[1]} ${table}.${key} = ${x[2]}`;
-    } else {
-    /** DEFAULT */
-      searchBy += ` ${table}.${key} ILIKE '%${x}%'`;
-    }
-  });
 
   Object.keys(data).map((key) => {
     const payload =
@@ -104,12 +80,20 @@ const getData = sqlGetDataHelper(
   (table = "school.td_student"),
   search,
   (limit = 6),
-  (offset = 1),
+  (offset = 1)
   // (orderBy = "userId")
 );
 
 const updateData = update((table = "school.td_student"), search, data);
 
-// console.log(updateData);
+console.log(updateData, "\n");
 console.log(getData);
 // console.log(sqlInsertHelper('student',data));
+
+const filter = {
+  tot: "46"
+}
+
+filter?.tut = parseInt(filter.tut)  
+
+console.log(filter.tut);
--- test.js
+++ test.js
@@ -1,22 +1,203 @@
-const data = [
+let arr1 = [
   {
-    userName: "dono",
-    age: "24",
+    kode_status: "001",
+    uraian: "Lengkap",
+    id_header: "5b43ff98-bfa0-0c16-e882-ac7dc951893b",
+    kode_ga: "01",
+    kd_jenis_aturan: "IE",
+    tema: "testing 1",
+    no_peraturan_menteri: "123",
+    status_peraturan: null,
+    nip_rekam: null,
+    nip_ubah: null,
+    waktu_ubah: null,
+    ur_ga: "Kementerian Perdagangan (Kemendag)",
+    ur_flag: "IMPOR & EKSPOR",
+    uraianfinal: "Kirim Data ke Admin KL",
+    kodeakhir: "002",
   },
   {
-    userName: "dani",
-    age: "21",
+    kode_status: "000",
+    uraian: "Draft",
+    id_header: "5b43ff98-bfa0-0c16-e882-ac7dc951893b",
+    kode_ga: "01",
+    kd_jenis_aturan: "IE",
+    tema: "testing 1",
+    no_peraturan_menteri: "123",
+    status_peraturan: null,
+    nip_rekam: null,
+    nip_ubah: null,
+    waktu_ubah: null,
+    ur_ga: "Kementerian Perdagangan (Kemendag)",
+    ur_flag: "IMPOR & EKSPOR",
+    uraianfinal: "Kirim Data ke Admin KL",
+    kodeakhir: "002",
   },
   {
-    userName: "dini",
-    age: "22",
+    kode_status: "002",
+    uraian: "Kirim Data ke Admin KL",
+    id_header: "5b43ff98-bfa0-0c16-e882-ac7dc951893b",
+    kode_ga: "01",
+    kd_jenis_aturan: "IE",
+    tema: "testing 1",
+    no_peraturan_menteri: "123",
+    status_peraturan: null,
+    nip_rekam: null,
+    nip_ubah: null,
+    waktu_ubah: null,
+    ur_ga: "Kementerian Perdagangan (Kemendag)",
+    ur_flag: "IMPOR & EKSPOR",
+    uraianfinal: "Kirim Data ke Admin KL",
+    kodeakhir: "002",
+  },
+  {
+    kode_status: "002",
+    uraian: "Kirim Data ke Admin KL",
+    id_header: "8aa32a7e-256f-11ec-f648-84f3063e95b3",
+    kode_ga: "01",
+    kd_jenis_aturan: "IE",
+    tema: "testing selaras 3",
+    no_peraturan_menteri: "123",
+    status_peraturan: null,
+    nip_rekam: null,
+    nip_ubah: null,
+    waktu_ubah: null,
+    ur_ga: "Kementerian Perdagangan (Kemendag)",
+    ur_flag: "IMPOR & EKSPOR",
+    uraianfinal: "Disetujui oleh Admin KL",
+    kodeakhir: "003",
+  },
+  {
+    kode_status: "000",
+    uraian: "Draft",
+    id_header: "8aa32a7e-256f-11ec-f648-84f3063e95b3",
+    kode_ga: "01",
+    kd_jenis_aturan: "IE",
+    tema: "testing selaras 3",
+    no_peraturan_menteri: "123",
+    status_peraturan: null,
+    nip_rekam: null,
+    nip_ubah: null,
+    waktu_ubah: null,
+    ur_ga: "Kementerian Perdagangan (Kemendag)",
+    ur_flag: "IMPOR & EKSPOR",
+    uraianfinal: "Disetujui oleh Admin KL",
+    kodeakhir: "003",
+  },
+  {
+    kode_status: "001",
+    uraian: "Lengkap",
+    id_header: "8aa32a7e-256f-11ec-f648-84f3063e95b3",
+    kode_ga: "01",
+    kd_jenis_aturan: "IE",
+    tema: "testing selaras 3",
+    no_peraturan_menteri: "123",
+    status_peraturan: null,
+    nip_rekam: null,
+    nip_ubah: null,
+    waktu_ubah: null,
+    ur_ga: "Kementerian Perdagangan (Kemendag)",
+    ur_flag: "IMPOR & EKSPOR",
+    uraianfinal: "Disetujui oleh Admin KL",
+    kodeakhir: "003",
+  },
+  {
+    kode_status: "003",
+    uraian: "Disetujui oleh Admin KL",
+    id_header: "8aa32a7e-256f-11ec-f648-84f3063e95b3",
+    kode_ga: "01",
+    kd_jenis_aturan: "IE",
+    tema: "testing selaras 3",
+    no_peraturan_menteri: "123",
+    status_peraturan: null,
+    nip_rekam: null,
+    nip_ubah: null,
+    waktu_ubah: null,
+    ur_ga: "Kementerian Perdagangan (Kemendag)",
+    ur_flag: "IMPOR & EKSPOR",
+    uraianfinal: "Disetujui oleh Admin KL",
+    kodeakhir: "003",
+  },
+  {
+    kode_status: "002",
+    uraian: "Kirim Data ke Admin KL",
+    id_header: "121b5826-e4c7-f70b-1aa5-f3b869046528",
+    kode_ga: "01",
+    kd_jenis_aturan: "IE",
+    tema: "testing selaras 2",
+    no_peraturan_menteri: "123",
+    status_peraturan: null,
+    nip_rekam: null,
+    nip_ubah: null,
+    waktu_ubah: null,
+    ur_ga: "Kementerian Perdagangan (Kemendag)",
+    ur_flag: "IMPOR & EKSPOR",
+    uraianfinal: "Kirim Data ke Admin KL",
+    kodeakhir: "002",
+  },
+  {
+    kode_status: "002",
+    uraian: "Kirim Data ke Admin KL",
+    id_header: "121b5826-e4c7-f70b-1aa5-f3b869046528",
+    kode_ga: "01",
+    kd_jenis_aturan: "IE",
+    tema: "testing selaras 2",
+    no_peraturan_menteri: "123",
+    status_peraturan: null,
+    nip_rekam: null,
+    nip_ubah: null,
+    waktu_ubah: null,
+    ur_ga: "Kementerian Perdagangan (Kemendag)",
+    ur_flag: "IMPOR & EKSPOR",
+    uraianfinal: "Kirim Data ke Admin KL",
+    kodeakhir: "002",
+  },
+  {
+    kode_status: "002",
+    uraian: "Kirim Data ke Admin KL",
+    id_header: "121b5826-e4c7-f70b-1aa5-f3b869046528",
+    kode_ga: "01",
+    kd_jenis_aturan: "IE",
+    tema: "testing selaras 2",
+    no_peraturan_menteri: "123",
+    status_peraturan: null,
+    nip_rekam: null,
+    nip_ubah: null,
+    waktu_ubah: null,
+    ur_ga: "Kementerian Perdagangan (Kemendag)",
+    ur_flag: "IMPOR & EKSPOR",
+    uraianfinal: "Kirim Data ke Admin KL",
+    kodeakhir: "002",
   },
 ];
 
-let userName = ''
+const arry = arr1.map((e) => {
+  return e.id_header;
+});
 
-data.map((x) => {
-    x.age === '20' ? userName = x.userName : ''
-}) 
+const uniqueVal = [...new Set(arry)];
 
-console.log(userName);
+let output = [];
+
+for (let i = 0; i < uniqueVal.length; i++) {
+  let array = [];
+
+  for (let j = 0; j < arr1.length; j++) {
+    if (arr1[j].id_header === uniqueVal[i]) {
+      array.push(arr1[j].kode_status);
+    }
+  }
+
+  const result = arr1.find((a) => {
+    if (
+      a.kode_status === array.sort()[array.length - 1] &&
+      a.id_header === uniqueVal[i]
+    ) {
+      return a;
+    }
+  });
+
+  output.push(result);
+}
+
+console.log(output);
